f3ce3c94763e4794685cf98c235c0467b4546dde
Gabriel Porto <gabrieljoserporto@gmail.com>
2022-03-24 20:54:47 -0300

    Final

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,49 +1,40 @@
-import React from 'react';
-import logo from './imagens/logo.png';
-import {Link} from 'react-router-dom';
+import React from "react";
+import logo from "./imagens/logo.png";
+import { Link } from "react-router-dom";
 
 function Header() {
-    return (
-        <nav className='navbar navbar-expand-sm bg-dark navbar-dark'>
-            <div className='container container-sm|md|lg|xl '>
-                <Link to='/'>
-                    <img
-                        className='img-fluid'
-                        id='headerlogo'
-                        src={logo}
-                        alt='logo'
-                        
-                    />
-                </Link>
-                
-                <ul className='navbar-nav'>
-                    <li className='nav-item'>  
-                        <a className='nav-link' >
-                            <Link to='/'>
-                                Home
-                            </Link>
-                        </a>
-                    </li>
-                    <li className='nav-item'>
-                        <a className='nav-link' >
-                            <Link to='/faq'>
-                                FAQ
-                            </Link>
-                            
-                        </a>
-                    </li>
-                    <li className='nav-item'>
-                        <a className='nav-link' >
-                            <Link to='/cadastro'>
-                                Cadastro
-                            </Link>
-                            
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-    );
+  return (
+    <nav className="navbar navbar-expand-sm bg-dark navbar-dark">
+      <div className="container container-sm|md|lg|xl ">
+        <Link to="/">
+          <img className="img-fluid" id="headerlogo" src={logo} alt="logo" />
+        </Link>
+
+        <ul className="navbar-nav">
+
+          <li className="nav-item">
+            <Link to="/" className="nav-link" style={{ textDecoration: "none"}}>
+              Home
+            </Link>
+          </li>
+
+          <li className="nav-item">
+
+            <Link to="/faq" className="nav-link" style={{ textDecoration: "none"}}>
+              FAQ
+            </Link>
+
+          </li>
+
+          <li className="nav-item">
+            <Link to="/cadastro" className="nav-link" style={{ textDecoration: "none"}}>
+                Cadastro
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
 
 export default Header;
