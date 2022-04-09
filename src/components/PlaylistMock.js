5b4314c7f49fae60e6a62bd1c95a3426f968b7aa
Gabriel Porto <gabrieljoserporto@gmail.com>
2022-04-09 17:08:54 -0300

    Pagina das Playlists-Estilo

--- src/components/PlaylistMock.js
+++ src/components/PlaylistMock.js
@@ -4,60 +4,29 @@ import playlistsMock from "../Data/playlistsMock";
 
 
 export default function PlaylistMock() {
-  return (
-    <div className="container">
-      <div className="row row-cols-4">
-        {playlistsMock.map((playlist) => {
-          return (
-            <Link to={`/playlists/${playlist.id}`}>
-                
-                <div className="col">
-                  <div
-                    className="card"
-                    style={{
-                      background:
-                        "linearGradient(rgba(0,0,0,0.3), rgba(0,0,0,0.2))",
-                    }}
-                  >
-                    <img
-                      className="card-user avatar avatar-large"
-                      src={playlist.Capa}
-                    />
-                  </div>
-                </div>
-            </Link>
-          );
-        })}
+  return ( 
+    <div className="container my-5" >
+      <div className="bg-dark rounded px-5 pt-3">
+        <h1 className="text-center mb-3 text-light">
+          Playlists pra melhorar o seu dia.
+        </h1>
+        <div className="row row-cols-4 m-0">
+          {playlistsMock.map((playlist) => {
+            return (
+              <div className="col pb-4 px-2 m-0">
+                <Link to={`/playlists/${playlist.id}`}>
+                  <img
+                    // id="capa"
+                    src={playlist.Capa}
+                    height={250}
+                    width={250}
+                  />
+                </Link>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
-    // <section className="gradient-custom">
-    //   <div className="container py-5 h-100">
-    //     <div className="row justify-content-center align-items-center h-100">
-    //       <div className="col-12 col-lg-9 col-xl-7">
-    //         <div className="card shadow-2-strong card-registration bg-dark">
-    //           <div className="row align-items-center justify-content-center h-100">
-    //             <h3 className="row justify-content-center align-self-center text-light">
-    //               Playlists para melhorar o seu dia!
-    //             </h3>
-    //           </div>
-
-    //           {/* <div>
-    //             <div className="row align-items-start">
-    //               <div className="col-3">Capa</div>
-    //               <div className="col-3">Capa</div>
-    //               <div className="col-3">Capa</div>
-    //               <div className="col-3">Capa</div>
-    //             </div>
-    //             <div className="row align-items-start">
-    //               <div className="col-3">Capa</div>
-    //               <div className="col-3">Capa</div>
-    //               <div className="col-3">Capa</div>
-    //               <div className="col-3">Capa</div>
-    //             </div> */}
-    //         </div>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </section>
   );
 }
